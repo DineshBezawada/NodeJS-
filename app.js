0f44d6a6011d67bc1acb9d03bfc35e578c8eee9a
DineshBezawada <dineshbezawada@gmail.com>
2025-02-07 18:59:43 +0530

    changes

---
+++ app.js
@@ -0,0 +1,15 @@
+require('./xyz');
+// const {text,calculateSum} = require('./calculate/sum');
+// import {calculateSum} from "./sum.js"
+
+const {calculateSum, calculateMultiply} = require("./calculate");
+
+
+const name = `Namste NodeJs`;
+var a= 10;
+var b = 20;
+
+console.log( globalThis === global );
+calculateSum(10,50);
+calculateMultiply(10,50);
+
---
+++ calculate/sum.js
@@ -0,0 +1,9 @@
+
+var text ='Helo World';
+
+ function calculateSum (a,b){
+    const sum = a+b;
+    console.log(sum);
+};
+
+module.exports={text,calculateSum}
